bb8cfb4c1075297e80c3c741b571e2ef2c60418b
zzlyung <124063972+zzlyung@users.noreply.github.com>
2024-12-28 14:04:34 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -33,11 +33,11 @@ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 # Add luci-app-amlogic
 #svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-svn co https://github.com/kenzok8/small/tree/master/luci-app-ssr-plus luci-app-ssr-plus
-svn co https://github.com/kenzok8/small/tree/master/luci-app-passwall luci-app-passwall
-svn co https://github.com/kenzok8/small/tree/master/luci-app-openclash luci-app-openclash
-svn co https://github.com/kenzok8/small/tree/master/luci-app-homeproxy luci-app-homeproxy
-svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-store luci-app-store
+svn co https://github.com/kenzok8/small/tree/master/luci-app-ssr-plus package/luci-app-ssr-plus
+svn co https://github.com/kenzok8/small/tree/master/luci-app-passwall package/luci-app-passwall
+svn co https://github.com/kenzok8/small/tree/master/luci-app-openclash package/luci-app-openclash
+svn co https://github.com/kenzok8/small/tree/master/luci-app-homeproxy package/luci-app-homeproxy
+svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-store package/luci-app-store
 
 # Fix runc version error
 # rm -rf ./feeds/packages/utils/runc/Makefile
